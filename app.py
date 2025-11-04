ad4d95366a1d3ea6dcaa7362c6aeacbc38a1ab3c
Gustavo Alves <gustavoalima14057@gmail.com>
2025-11-04 16:52:12 -0300

    abc

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
-a=1
-print(a+2)
+char = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+def calc(num):
+    for i in range(num):
+        print(char[i], end=" ")
+
+calc(4)
